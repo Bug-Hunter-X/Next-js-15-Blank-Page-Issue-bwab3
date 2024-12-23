800c1c6a92bf1f9c2fe297f0a3c151a273488bae
BugHunter-X <askzenai@gmail.com>
2024-12-24 00:36:05 +0100

    Add index.solution.js file

---
+++ index.solution.js
@@ -0,0 +1,12 @@
+```javascript
+// pages/index.js
+
+// Explicit return type added for clarity
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+    </div>
+  );
+}
+```
